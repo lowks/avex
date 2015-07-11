01e97d5dee7b94a1e0dbf65e087edd592d8a4c27
Jonas Trevisan <jonhkr@gmail.com>
2015-07-11 12:37:07 -0300

    Fix fns being registered multiple times

--- lib/avex/test.ex
+++ lib/avex/test.ex
@@ -4,23 +4,22 @@ defmodule Avex.Test do
   fields :optional, [:number, :expiry_date]
   fields :required, [:test, :holder]
 
-  # update :test, with: :update_test
-  # update :test, with: :update_test, args: ["hey"]
+  update :test, with: :update_test
+  update :test, with: :update_test, args: ["hey"]
   update :test, value when is_binary(value) do
-    String.upcase(value)
+    :ok
   end
   update :test, nil, do: nil
   # validate :test, format: ~r/^(\d{3,4})?$/
   # validate :test, included: ["list"]
   # validate :test, excluded: ["list"]
 
-  # validate :test do
-  #   if is_binary(value) do
-  #     :ok
-  #   else
-  #     {:error, "value is invalid"}
-  #   end
-  # end
+  validate :test, value when is_binary(value) do
+    :ok
+  end
+  validate :test, value when not is_binary(value) do
+    {:error, "value is invalid"}
+  end
 
   validate :test, value do
     :ok
