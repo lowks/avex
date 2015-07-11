5935dde3fdbbc1ddebe022814e9336550456cdde
Jonas Trevisan <jonhkr@gmail.com>
2015-07-11 12:21:50 -0300

    Allo guards in update functions

--- lib/avex/test.ex
+++ lib/avex/test.ex
@@ -6,18 +6,24 @@ defmodule Avex.Test do
 
   # update :test, with: :update_test
   # update :test, with: :update_test, args: ["hey"]
-  # update :test, do: String.upcase(value)
+  update :test, value when is_binary(value) do
+    String.upcase(value)
+  end
+  update :test, nil, do: nil
+  # validate :test, format: ~r/^(\d{3,4})?$/
+  # validate :test, included: ["list"]
+  # validate :test, excluded: ["list"]
 
-  validate :test, format: ~r/^(\d{3,4})?$/
-  validate :test, included: ["list"]
-  validate :test, excluded: ["list"]
+  # validate :test do
+  #   if is_binary(value) do
+  #     :ok
+  #   else
+  #     {:error, "value is invalid"}
+  #   end
+  # end
 
-  validate :test do
-    if is_binary(value) do
-      :ok
-    else
-      {:error, "value is invalid"}
-    end
+  validate :test, value do
+    :ok
   end
 
   def update_test(:ok, "hey") do
